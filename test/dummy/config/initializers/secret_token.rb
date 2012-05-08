ca2e9e2e2a9fe02728505005d0507ae0e6f3fd30
Felix Tjandrawibawa <cemeng@gmail.com>
2012-05-08 12:34:32 +1000

    Ah that fresh first commit!

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '165fb51e6ef0a41f287f47b7d64e55cafaafc19424b57d9c26b5495fd51a5e26d5913ec47c0d73a34445773b8937750ea31231b680312498037092a7e6ae2bb9'
